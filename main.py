be24d5286b618c3658a2aac74bf103f5ef253044
Jehovah-nissi <jehovahnissi6384@gmail.com>
2025-10-15 22:04:45 +0100

    added print report and dictionary sorting

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from stats import get_num_words, get_num_character
+from stats import get_num_words, get_chars_dict, sort_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
-        file_content = f.read()
-    return file_content
+        return f.read()
 
 def main():
-    book_text = get_book_text("/home/hova/workspace/github.com/HOVA63/bookbot/books/frankenstein.txt")
+    book_path = "/home/hova/workspace/github.com/HOVA63/bookbot/books/frankenstein.txt"
+    book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
+    chars_dict = get_chars_dict(book_text)
+    sorted_dict = sort_dict(chars_dict)
+    print_report(num_words, sorted_dict, book_path)
+    
+def print_report(num_words, sorted_dict, book_path):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(get_num_character(book_text))
+    print(f"--------- Character Count -------")
+    for item in sorted_dict:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,22 @@
-def get_num_words(file_content):
-    return len(file_content.split())
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-def get_num_character(file_content):
-    file_content = file_content.lower()
-    characters = {}
-    for i in file_content:
-        characters[i] = characters.get(i, 0) + 1
-    return characters
+def get_chars_dict(text):
+    chars = {}
+    for i in text:
+        lowered = i.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def sort_dict(char_dict):
+    sorted_dict_list = []
+    for key, value in char_dict.items():
+        sorted_dict_list.append({"char": key, "num": value})
+    def sort_on(items):
+        return items["num"]
+    sorted_dict_list.sort(reverse=True, key=sort_on)
+    return sorted_dict_list
