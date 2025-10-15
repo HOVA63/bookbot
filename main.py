9ca6d2f30bc09b82a2e551523fc662740abdb9bf
Jehovah-nissi <jehovahnissi6384@gmail.com>
2025-10-15 02:33:40 +0100

    created character counter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_character
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,5 +9,6 @@ def main():
     book_text = get_book_text("/home/hova/workspace/github.com/HOVA63/bookbot/books/frankenstein.txt")
     num_words = get_num_words(book_text)
     print(f"Found {num_words} total words")
+    print(get_num_character(book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,9 @@
 def get_num_words(file_content):
     return len(file_content.split())
 
+def get_num_character(file_content):
+    file_content = file_content.lower()
+    characters = {}
+    for i in file_content:
+        characters[i] = characters.get(i, 0) + 1
+    return characters
